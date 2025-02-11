c51c1c9ff642d79efbcda72d16bee511014cc6d7
Umar Hassan <umar@finehomeint.com>
2025-02-11 18:42:41 +0400

    3 More Test Cases

--- src/test/java/TestCase1.java
+++ src/test/java/TestCase1.java
@@ -3,7 +3,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v130.network.model.Response;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -16,17 +19,21 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import javax.sound.sampled.Control;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.security.Key;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
+import java.util.Set;
 
 
-public class TestCase1 extends RandomValuesGenerator{
+public class TestCase1 extends RandomValuesGenerator {
 
     WebDriver driver = new EdgeDriver();
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     Actions act = new Actions(driver);
+
     public String generatenewDateTime(){
         String dat = new Date().toString().replaceAll(" ", "").replaceAll(  ":", "") + "@gmail.com";
         return dat;
@@ -36,9 +43,10 @@ public class TestCase1 extends RandomValuesGenerator{
     public void classSetup(){
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 //        driver.manage().window().maximize();
-
     }
+
     //Test Case 1 [Registration]
+
     @Test(priority = 1)
     public void Registration() {
         driver.get("https://tutorialsninja.com/demo/");
@@ -55,14 +63,15 @@ public class TestCase1 extends RandomValuesGenerator{
         driver.findElement(By.xpath("/html/body/div[2]/div/div/form/div/div/input[2]")).click();
         String text = "Your Account Has Been !";
         Assert.assertNotEquals(driver.findElement(By.xpath("/html/body/div[2]/div/div/h1")).getText(), text);
-
         //Next Page
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/a")).click();
         Assert.assertTrue(driver.findElement(By.linkText("Logout")).isDisplayed());
     }
+
     //Test Case 2 [Newsletter Subscription]
-        @Test(priority = 2)
-        public void Subscription(){
+
+    @Test(priority  = 2)
+    public void Subscription(){
         WebElement subscribe = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Subscribe / unsubscribe to newsletter")));
         subscribe.click();
         String title = "Newsletter Subscription";
@@ -74,6 +83,7 @@ public class TestCase1 extends RandomValuesGenerator{
     }
 
     //Test Case 3 [Random Address Generation]
+
     @Test(priority = 3)
     public void AddressBook() throws InterruptedException {
         driver.findElement(By.linkText("Address Book")).click();
@@ -96,7 +106,6 @@ public class TestCase1 extends RandomValuesGenerator{
 
         driver.findElement(By.id("input-address-2")).sendKeys(RandomAreaGenerator());
         driver.findElement(By.id("input-city")).sendKeys("Abu Dhabi");
-
         driver.findElement(By.name("postcode")).sendKeys("23243");
 
         //Dropdown Fields (Country and Region/State)
@@ -109,10 +118,11 @@ public class TestCase1 extends RandomValuesGenerator{
         driver.findElement(By.xpath("/html/body/div[2]/div/div/form/div/div[2]/input")).click();
         SoftAssert soft = new SoftAssert();
         soft.assertEquals(driver.getCurrentUrl(), "https://tutorialsninja.com/demo/index.php?route=checkout/cart", "Title match");
-
+        System.out.println(driver.getWindowHandles());
     }
 
     //Test Case 4 [Filling the Random Addresses]
+
     @Test(priority = 4)
     public void addressIteration() throws InterruptedException {
         driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/table/tbody/tr/td[2]/a[1]")).click();
@@ -121,6 +131,7 @@ public class TestCase1 extends RandomValuesGenerator{
     }}
 
     //Test Case 5 [Adding Item to Cart]
+
     @Test(priority = 5)
     public void DesktopAddtoCart()   throws InterruptedException {
         Actions actions = new Actions(driver);
@@ -129,13 +140,11 @@ public class TestCase1 extends RandomValuesGenerator{
         WebElement macselect = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/nav/div[2]/ul/li[1]/div/div/ul/li[2]/a")));
         macselect.click();
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div[2]/button[2]")).click();
-        //driver.wait(2000);
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div[2]/button[1]")).click();
 
         Actions act = new Actions(driver);
         WebElement itemcarts = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/header/div/div/div[3]/div/button")));
         itemcarts.click();
-        //act.moveToElement(itemcarts).perform();
 
         //Failed Test Case Step
         try {
@@ -149,6 +158,9 @@ public class TestCase1 extends RandomValuesGenerator{
         }
         //Checkout click
     }
+
+    //Test Case 6 [Adding Item to Cart]
+
     @Test(priority = 6)
     public void ComponentsAddtoCart(){
         Actions act = new Actions(driver);
@@ -190,12 +202,11 @@ public class TestCase1 extends RandomValuesGenerator{
         WebElement elementDisplay = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div[1]/div[2]/div/h3")));
 
         Assert.assertTrue(elementDisplay.isDisplayed());
-        //Actions doubleclick = new Actions(driver);
-
-        //WebElement doubl = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[3]/div/form/div/table/thead/tr/td[4]")));
-        //doubleclick.moveToElement(doubl).doubleClick().click();
 
     }
+
+    //Test Case 7 [Modifying the Cart Order]
+
     @Test(priority = 7)
     public void CartRefinement() throws InterruptedException {
         WebElement cartplusitem1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div/form/div/table/tbody/tr[1]/td[4]/div/input")));
@@ -207,16 +218,75 @@ public class TestCase1 extends RandomValuesGenerator{
         act.moveToElement(cartplusitem2).click().keyDown(Keys.CONTROL).sendKeys("a").sendKeys("v").keyUp(Keys.CONTROL).perform();
 
     }
+
+    //Test Case 8 [Traversing Back to Search]
+
     @Test(priority = 8)
     public void continueShopping(){
         WebElement continueShop = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[2]/div/div[3]/div[1]/a")));
         continueShop.click();
     }
 
+    //Test Case 9 [Opening a New Tab]
+
+    @Test(priority = 9)
+    public void newWindow() throws AWTException, InterruptedException {
+
+        WebElement components = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/nav/div[2]/ul/li[4]/a")));
+        String drive = driver.getWindowHandle();
+        act.moveToElement(components).perform();
+        act.contextClick(components).perform();
+        //Switching to a new Tab
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_DOWN);  // Move down in the right-click menu
+        robot.keyPress(KeyEvent.VK_ENTER);
+        Set<String> allwindowhandles = driver.getWindowHandles();
+
+        for(String wind: allwindowhandles){
+            if(!wind.equals(drive)){
+                driver.switchTo().window(wind);
+                System.out.println("Switched to new tab: " + driver.getTitle());
+            }
+        }
+        /*WebElement addTablet = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div[2]/button[1]")));
+        addTablet.click();
+        driver.findElement(By.xpath("/html/body/header/div/div/div[3]/div/button")).click();
+        driver.findElement(By.xpath("/html/body/header/div/div/div[3]/div/ul/li[2]/div/p/a[1]/strong")).click();*/
+    }
+    @Test(priority = 10)
+    public void ContactUs(){
+        WebElement contactUs = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Contact Us")));
+        contactUs.click();
+        WebElement inquiry = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/form/fieldset/div[3]/div/textarea")));
+        inquiry.sendKeys("The website is amazing!");
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/form/div/div/input")).click();
+        driver.findElement(By.linkText("Continue")).click();
+    }
+
+    @Test(priority = 11)
+    public void ProductReturns(){
+        WebElement returnProduct = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Returns")));
+        returnProduct.click();
+        WebElement orderID = wait.until(ExpectedConditions.elementToBeClickable(By.name("order_id")));
+        orderID.sendKeys("24");
+        WebElement productName = wait.until(ExpectedConditions.elementToBeClickable(By.name("product")));
+        productName.sendKeys("Samsung Tablet");
+        WebElement productCode = wait.until(ExpectedConditions.elementToBeClickable(By.name("model")));
+        productCode.sendKeys("2");
+        Assert.assertEquals(driver.findElement(By.name("model")).getText(), 2);
+        WebElement toggle = wait.until(ExpectedConditions.elementToBeClickable(By.name("return_reason_id")));
+        toggle.click();
+        WebElement otherdetails = wait.until(ExpectedConditions.elementToBeClickable(By.id("input-comment")));
+        otherdetails.sendKeys("Superb Product it is");
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/form/div/div[2]/input")).click();
+
+
+    }
+    //Test Case 10 [Quitting the Window]1
 
     @AfterClass
     public void quitWindow(){
      System.out.print("Class Executed!");
-        //driver.quit();
+     //driver.quit();
     }
 }
